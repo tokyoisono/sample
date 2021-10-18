a6707e21e514e6d5e80b14aef58f404acfad7576
isono <h-i@v-apex.co.jp>
2021-10-18 16:14:50 +0900

    Updates

---
+++ static/js/main.9a86fb1f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpsample=this.webpackJsonpsample||[]).push([[0],{9:function(e,s,t){"use strict";t.r(s);var n=t(3),r=t.n(n),c=(t(1),t(0)),i=function(){var e=new Date;return Object(c.jsxs)("div",{children:[Object(c.jsxs)("p",{children:["Hello world, it is ",e.toString()]}),Object(c.jsxs)("p",{children:[10," plus ",20," is ",30]})]})};r.a.render(Object(c.jsx)(i,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.9a86fb1f.chunk.js.map
